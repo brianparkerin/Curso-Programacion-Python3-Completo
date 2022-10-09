7a35a7e318368a54f91de46a3f42f16b09e00f98
Brian Parker <60494113+brianparkerin@users.noreply.github.com>
2022-10-09 14:02:34 +0000

    new Changes Tablas de verdad

---
+++ Ejercicios/Ejercicios.py
@@ -0,0 +1,43 @@
+# Ejercicios & Problems to solve with python:
+
+# Ejercicio 1:
+# construir la Tabla de la Disyuncion "or":
+
+booleanos = [False, True]
+
+print('X\tY\tX or Y')
+print('_'*26)
+
+for X in booleanos:
+  for Y in booleanos:
+    print('{} \t {} \t {}'.format(X,Y,X or Y))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
